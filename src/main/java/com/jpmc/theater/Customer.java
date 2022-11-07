3dcb43bdb60a66cdd77aac45d43e4706eae150cd
shellyneira <sneirabastos@miu.edu>
2022-11-07 08:09:59 -0500

    [refactor] transform customer in immutable using records

--- src/main/java/com/jpmc/theater/Customer.java
+++ src/main/java/com/jpmc/theater/Customer.java
@@ -1,39 +1,30 @@
 package com.jpmc.theater;
 
 import java.util.Objects;
+import java.util.UUID;
 
-public class Customer {
-
-    private String name;
-
-    private String id;
-
-    /**
-     * @param name customer name
-     * @param id customer id
-     */
-    public Customer(String name, String id) {
-        this.id = id; // NOTE - id is not used anywhere at the moment
-
-        this.name = name;
-
-        }
+public record Customer(String identifier, String name) {
+    public Customer(final String name) {
+        this(UUID.randomUUID().toString(), name);
+    }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Customer)) return false;
-        Customer customer = (Customer) o;
-        return Objects.equals(name, customer.name) && Objects.equals(id, customer.id);
+    public boolean equals(final Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        final Customer customer = (Customer) o;
+        return Objects.equals(identifier, customer.identifier) && Objects.equals(name, customer.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, id);
+        return Objects.hash(name, identifier);
     }
 
     @Override
     public String toString() {
-        return "name: " + name;
+        return "Customer{" + "identifier='" + identifier + '\'' + ", name='" + name + '\'' + '}';
     }
 }
