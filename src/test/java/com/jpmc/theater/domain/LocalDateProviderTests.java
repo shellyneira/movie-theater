c8c2521aad9fda80fe047f32a7894b24f01b051b
shellyneira <sneirabastos@miu.edu>
2022-11-07 09:16:17 -0500

    [refactor] move junit test to domain directory

---
+++ src/test/java/com/jpmc/theater/domain/LocalDateProviderTests.java
@@ -0,0 +1,11 @@
+package com.jpmc.theater.domain;
+
+import com.jpmc.theater.LocalDateProvider;
+import org.junit.jupiter.api.Test;
+
+public class LocalDateProviderTests {
+    @Test
+    void makeSureCurrentTime() {
+        System.out.println( "current time is - " + LocalDateProvider.singleton().currentDate() );
+    }
+}
